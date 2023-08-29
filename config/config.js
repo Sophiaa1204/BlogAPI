1b44606753dcc9d92108d25a0fd939602395c222
Sophiaa1204 <SihanWangSophia@gmail.com>
2023-08-28 20:12:33 -0600

    Add config/config.js

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  db: {
+    host: 'localhost',
+    user: 'root',
+    password: 'asdf@520',
+    database: 'blog',
+  },
+  token: {
+    secretKey: 'A%&(SD)(AS*DAV^AD%*^VA%)(D^',
+  },
+}
