503de87c42699dea87c5991babf71b08fd52e04b
Sophiaa1204 <SihanWangSophia@gmail.com>
2023-09-22 20:12:34 -0600

    Add routes/category.js

---
+++ routes/category.js
@@ -0,0 +1,11 @@
+var express = require('express')
+var router = express.Router()
+const validate = require('../middlewares/validate')
+const { categoryModel } = require('../model')
+const { categoryController } = require('../controllers')
+router.post('/', validate(categoryModel.schema), categoryController.create)
+router.get('/:id', validate(['id']), categoryController.getInfoById)
+router.put('/:id', validate(['id', ...categoryModel.schema]), categoryController.updateInfoById)
+router.delete('/:id', validate(['id']), categoryController.deleteInfoById)
+router.get('/list/get', categoryController.getList)
+module.exports = router
