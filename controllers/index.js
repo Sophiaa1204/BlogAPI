a1d9068473bbdf183c7a1e27c667ef7cfa5ee113
Sophiaa1204 <SihanWangSophia@gmail.com>
2024-01-07 20:10:35 -0600

    Add index.js

---
+++ controllers/index.js
@@ -0,0 +1,6 @@
+module.exports.userController = require('./user.controller')
+module.exports.categoryController = require('./category.controller')
+module.exports.articleController = require('./article.controller')
+module.exports.likeController = require('./like.controller')
+module.exports.commentController = require('./comment.controller')
+module.exports.followerController = require('./follower.controller')
