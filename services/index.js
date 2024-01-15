409e956de3688d59e2a9421bce9e6ae730339d02
Sophiaa1204 <SihanWangSophia@gmail.com>
2024-01-14 20:10:57 -0600

    Add index.js

---
+++ services/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  userService: require('./user.service'),
+  categoryService: require('./category.service'),
+  articleService: require('./article.service'),
+  likeService: require('./like.service'),
+  commentService: require('./comment.service'),
+  followerService: require('./follower.service'),
+}
